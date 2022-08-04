04795f70a2f3cb34090b00c682578bb32581c707
iglesiasgustavo <gustavoiglesiassantos2@gmail.com>
2022-08-03 22:27:10 -0300

    Atividade Jogo Corrida

---
+++ MotorDeFusca.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TarefaJogoCorrida;
+
+/**
+ *
+ * @author Admin
+ */
+public class MotorDeFusca implements IMotor{
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public String acelerar(){
+        return "Vrum..";
+    }
+    
+}
